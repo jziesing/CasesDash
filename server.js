e4946a2a853a3511969282e31e7addbf257b1224
jack <jziesing@gmail.com>
2016-03-07 21:50:32 -0800

    using static

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.use(express.static("./client"));
-
-app.get("/", function(req, res) {
-    res.sendFile("./client/index.html");
-});
+app.use('/', express.static(__dirname + '/client'));
 
 // Start Server
 app.listen(port, function () {
